fd0a10d9dc84a8424e03f700c63dceba6bf2e186
Jose Velasco <jose.juarez.velasco@gmail.com>
2021-05-11 03:01:03 -0700

    implemented a reusable splineAreaSeriesChartComponent
    thermometerChartComponent will be deprecated to be replaced by the reusable splineAreaSeriesChartComponent.
    Created Interface for the new reusable chart component so it is easier to know what values are required to
    reuse the component in other parts of the aplication. Added dummy data for testing the chart component to be removed
    when needed. Replaced deprecated thermometerChartCompnent from dashboardCharts component.

--- src/app/components/dashboard-charts/dashboard-charts.component.ts
+++ src/app/components/dashboard-charts/dashboard-charts.component.ts
@@ -1,10 +1,55 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ChartsDataService } from "../../shared/services/charts-data.service";
+import { Subscription } from "rxjs";
+import { SplineAreaSeriesRequiredValues } from "../../shared/models/spline-area-series-required-values.model";
 
 @Component({
     selector: "ns-dashboard-charts",
     templateUrl: "./dashboard-charts.component.html",
     styleUrls: ["./dashboard-charts.component.scss"]
 })
-export class DashboardChartsComponent {
-    constructor() {}
+export class DashboardChartsComponent implements OnInit, OnDestroy {
+    private _humidityDataSub: Subscription;
+    private _humidityChartData: SplineAreaSeriesRequiredValues;
+    private _temperatureDataSub: Subscription;
+    private _temperatureChartData: SplineAreaSeriesRequiredValues;
+    private _luminosityDataSub: Subscription;
+    private _luminosityChartData: SplineAreaSeriesRequiredValues;
+    constructor(private chartsDataService: ChartsDataService) {}
+
+    ngOnInit() {
+        this._humidityDataSub = this.chartsDataService.humidityData
+            .subscribe((humidityData) => {
+                this.humidityChartData = humidityData;
+        });
+        this._temperatureDataSub = this.chartsDataService.temperatureData
+            .subscribe((temperatureData) => {
+                this.temperatureChartData = temperatureData;
+        });
+        this._luminosityDataSub = this.chartsDataService.luminosityData
+            .subscribe((luminosityData) => {
+                this.luminosityChartData = luminosityData;
+            });
+    }
+
+    get humidityChartData(): SplineAreaSeriesRequiredValues { return this._humidityChartData; }
+    set humidityChartData(newValues: SplineAreaSeriesRequiredValues) {
+        this._humidityChartData = newValues;
+    }
+
+    get temperatureChartData(): SplineAreaSeriesRequiredValues { return this._temperatureChartData; }
+    set temperatureChartData(newPropertyValues: SplineAreaSeriesRequiredValues) {
+        this._temperatureChartData = newPropertyValues;
+    }
+
+    get luminosityChartData(): SplineAreaSeriesRequiredValues { return this._luminosityChartData; }
+    set luminosityChartData(newPropertyValues: SplineAreaSeriesRequiredValues) {
+        this._luminosityChartData = newPropertyValues;
+    }
+
+    ngOnDestroy() {
+        this._humidityDataSub.unsubscribe();
+        this._temperatureDataSub.unsubscribe();
+        this._luminosityDataSub.unsubscribe();
+    }
 }
--- src/app/shared/services/charts-data.service.ts
+++ src/app/shared/services/charts-data.service.ts
@@ -1,20 +1,92 @@
 import { Injectable } from "@angular/core";
+import { Color } from "@nativescript/core/color";
 import { Temperature } from "../models/temperature.model";
+import { SplineAreaSeriesRequiredValues, SplineAreaSeriesChartDataItem } from "../models/spline-area-series-required-values.model";
 import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class ChartsDataService {
-    private _temperatureData = new BehaviorSubject<Temperature[]>(null);
+    private _humidityData = new BehaviorSubject<SplineAreaSeriesRequiredValues>(null);
+    private _temperatureData = new BehaviorSubject<SplineAreaSeriesRequiredValues>(null);
+    private _luminosityData = new BehaviorSubject<SplineAreaSeriesRequiredValues>(null);
+
+    testHumidityData: SplineAreaSeriesChartDataItem[] = [
+        {
+            dataValue: 76,
+            date: new Date(2021, 9, 30, 0),
+            timeStamp: new Date(2021, 9, 30, 0).getTime()
+        },
+        {
+            dataValue: 66,
+            date: new Date(2021, 9, 30, 23),
+            timeStamp: new Date(2021, 9, 30, 23).getTime()
+        },
+        {
+            dataValue: 93,
+            date: new Date(2021, 10, 1, 5),
+            timeStamp: new Date(2021, 10, 1, 5).getTime()
+        },
+        {
+            dataValue: 84,
+            date: new Date(2021, 10, 1, 11),
+            timeStamp: new Date(2021, 10, 1, 11).getTime()
+        },
+        {
+            dataValue: 50,
+            date: new Date(2021, 10, 1, 17),
+            timeStamp: new Date(2021, 10, 1, 17).getTime()
+        },
+        {
+            dataValue: 55,
+            date: new Date(2021, 10, 1, 23),
+            timeStamp: new Date(2021, 10, 1, 23).getTime()
+        },
+        {
+            dataValue: 89,
+            date: new Date(2021, 10, 2, 5),
+            timeStamp: new Date(2021, 10, 2, 5).getTime()
+        },
+        {
+            dataValue: 21,
+            date: new Date(2021, 10, 3, 5),
+            timeStamp: new Date(2021, 10, 3, 5).getTime()
+        },
+        {
+            dataValue: 99,
+            date: new Date(2021, 10, 4, 5),
+            timeStamp: new Date(2021, 10, 4, 5).getTime()
+        },
+    ];
+
+
     constructor() {
         this.fetchTemperatureData();
+        this.fetchHumidityData();
+        this.fetchLuminosityData();
     }
 
-    get temperatureData(): Observable<Temperature[]>  {
+    get temperatureData(): Observable<SplineAreaSeriesRequiredValues>  {
         return this._temperatureData.asObservable();
     }
 
+    get humidityData(): Observable<SplineAreaSeriesRequiredValues> {
+        return this._humidityData.asObservable();
+    }
+
+    get luminosityData(): Observable<SplineAreaSeriesRequiredValues> {
+        return this._luminosityData.asObservable();
+    }
+
     fetchTemperatureData(): void {
-        this._temperatureData.next(this.getDummyTempDataArray());
+        this._temperatureData.next(this.getDummyTemperatureData());
+    }
+
+    fetchHumidityData(): void {
+        this._humidityData.next(this.getDummyHumidityData());
+    }
+
+    fetchLuminosityData(): void {
+        this._luminosityData.next(this.getDummySunData());
     }
 
     private getDummyTempDataArray(): Temperature[] {
@@ -36,5 +108,37 @@ export class ChartsDataService {
             new Temperature(65, new Date(2021, 10, 3, 23)),
             new Temperature(65, new Date(2021, 10, 4, 5)),
         ];
+    }
+
+    private getDummyHumidityData(): SplineAreaSeriesRequiredValues{
+        return {
+            dataItems: this.testHumidityData,
+            unitsSymbol: "％",
+            splineAreaProperties: {
+                legendTitle: "Humidity",
+                CurveBaseColor: new Color("#1D4985")
+            }
+        };
+    }
+
+    private getDummyTemperatureData(): SplineAreaSeriesRequiredValues{
+        return {
+            dataItems: this.testHumidityData,
+            unitsSymbol: "°F",
+            splineAreaProperties: {
+                legendTitle: "Temperature"
+            }
+        };
+    }
+
+    private getDummySunData(): SplineAreaSeriesRequiredValues {
+        return {
+            dataItems: this.testHumidityData,
+            unitsSymbol: "watts",
+            splineAreaProperties: {
+                legendTitle: "luminosity",
+                CurveBaseColor: new Color("#EBDB7A")
+            }
+        }
     }
 }
