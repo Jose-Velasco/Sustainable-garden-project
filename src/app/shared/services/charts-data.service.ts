9759b37eea8950341bf8c91496d200ab9df6250a
Jose Velasco <jose.juarez.velasco@gmail.com>
2021-07-29 04:16:50 -0700

    Removed no longer needed commented out code

--- src/app/shared/services/charts-data.service.ts
+++ src/app/shared/services/charts-data.service.ts
@@ -33,68 +33,16 @@ export class ChartsDataService implements OnDestroy {
     }
 
     initializeChartServiceData(): void {
-        this.setChartData(this.parseIncomingSensorData(this.sensorsReadingsService.getSensorsReadingsData()));
+        this.setChartData(this.parseIncomingSensorsDataToForCharts(this.sensorsReadingsService.getSensorsReadingsData()));
         this._sensorsReadingsDataChangedSub = this.sensorsReadingsService.sensorsReadingsDataChanged
         .subscribe((sensorsReadingsData) => {
-            this.setChartData(this.parseIncomingSensorData(sensorsReadingsData));
+            this.setChartData(this.parseIncomingSensorsDataToForCharts(sensorsReadingsData));
         });
     }
 
-    // private parseIncomingSensorData(newSensorsReadingsData: SensorReading[]): BaseContinuousGraphRequiredProperties[] {
-    //     let charts: BaseContinuousGraphRequiredProperties[] = [];
-    //     const testCharts = new Map<string, Record<string, any>>();
-    //     newSensorsReadingsData.forEach(sensorReading => {
-    //         const numberOfReadings = Object.keys(sensorReading.reading).length;
-    //         for (let i = 0; i < numberOfReadings; i++) {
-    //             const currentReadingKey = Object.keys(sensorReading.reading)[i];
-    //             const chartKey = `${sensorReading.sensor.id}${currentReadingKey}`;
-    //             if (testCharts.has(chartKey)) {
-    //                 // let date = this.dateOffsetHours(sensorReading.time_of_reading, 7);
-    //                 // let timestamp = date.getTime();
-    //                 const newContinuosGraphDataItem = this.sensorsReadingsService.generateContinuosGraphDataItem(
-    //                     sensorReading.time_of_reading, sensorReading.reading[currentReadingKey]
-    //                 );
-    //                 testCharts.get(chartKey).dataItems.push(newContinuosGraphDataItem);
-    //             } else {
-    //                 const chart: {
-    //                     chartTitle: string,
-    //                     legendTitle: string,
-    //                     unitsSymbol:string,
-    //                     dataItems: ObservableArray<ContinuousGraphDataItem>[]
-    //                 } = {
-    //                     chartTitle: sensorReading.sensor.sensor_name,
-    //                     legendTitle: currentReadingKey,
-    //                     // change once done with test
-    //                     unitsSymbol: "",
-    //                     dataItems: []
-    //                 };
-    //                 testCharts.set(chartKey, chart);
-    //             }
-    //         }
-    //     });
-
-    //     testCharts.forEach((chartData: {
-    //         chartTitle: string,
-    //         legendTitle: string,
-    //         unitsSymbol:string,
-    //         dataItems: ObservableArray<ContinuousGraphDataItem>[]
-    //         }) => {
-    //         const newChart: BaseContinuousGraphRequiredProperties = {
-    //             dataItems: chartData.dataItems,
-    //             unitsSymbol: chartData.unitsSymbol,
-    //             splineAreaProperties: {
-    //                 chartTitle: chartData.chartTitle,
-    //                 legendTitle: chartData.legendTitle
-    //             }
-    //         };
-    //         charts.push(newChart);
-    //     });
-
-    //     return charts;
-    // }
-    private parseIncomingSensorData(newSensorsReadingsData: SensorReading[]): BaseContinuousGraphRequiredProperties[] {
+    private parseIncomingSensorsDataToForCharts(newSensorsReadingsData: SensorReading[]): BaseContinuousGraphRequiredProperties[] {
         const charts: BaseContinuousGraphRequiredProperties[] = [];
-        // This maps a chart's index position in the charts array
+        // string referencing a chart's index in charts array
         const keyToIndexInChartsArray = new Map<string, number>();
         let rearIndex = 0;
         newSensorsReadingsData.forEach(sensorReading => {
@@ -106,32 +54,6 @@ export class ChartsDataService implements OnDestroy {
                 // utilize sensor's id and the user given name of sensor because
                 // sensors names are not unique
                 const chartKey = `${sensorReading.sensor.id}${currentReadingKey}`;
-                // check if chart already has been already added
-                // if (keyToIndexInChartsArray.has(chartKey)) {
-                //     const newContinuosGraphDataItem = this.sensorsReadingsService.generateContinuosGraphDataItem(
-                //         sensorReading.time_of_reading, sensorReading.reading[currentReadingKey]
-                //     );
-                //     charts[keyToIndexInChartsArray.get(chartKey)].dataItems.push(newContinuosGraphDataItem);
-                // // adds chart
-                // } else {
-                //     // This maps a new chart's index position in the charts array
-                //     // to have a reference to it chart by a key
-                //     keyToIndexInChartsArray.set(chartKey, rearIndex);
-                //     // updates the reference to the rear index in the charts array
-                //     rearIndex++;
-                //     const chart: BaseContinuousGraphRequiredProperties = {
-                //         dataItems: new ObservableArray<ContinuousGraphDataItem>(),
-                //         // change once done with test
-                //         unitsSymbol: "",
-                //         splineAreaProperties: {
-                //             chartTitle: sensorReading.sensor.sensor_name,
-                //             legendTitle: currentReadingKey,
-                //         }
-                //     };
-                //     // add new chart
-                //     charts.push(chart);
-                // }
-
                 // adds chart
                 if (!keyToIndexInChartsArray.has(chartKey)) {
                     // This maps a new chart's index position in the charts array
