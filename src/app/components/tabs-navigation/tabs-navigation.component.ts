d17be151b6ba9f5e5b828d79ab1c710a06a88d1e
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2021-10-14 11:03:30 -0700

    Documented everything in tabs-navigation

--- src/app/components/tabs-navigation/tabs-navigation.component.ts
+++ src/app/components/tabs-navigation/tabs-navigation.component.ts
@@ -3,31 +3,43 @@ import { ActivatedRoute } from "@angular/router";
 import { RouterExtensions } from "@nativescript/angular";
 import { Page, isAndroid } from "@nativescript/core";
 
+// decorator, tells angular and nativescript what component we are talking about
 @Component({
-    selector: "ns-tabs-navigation",
-    templateUrl: "./tabs-navigation.component.html",
-    styleUrls: ["./tabs-navigation.component.scss"]
+    selector: "ns-tabs-navigation", //where in nativescript is this component
+    templateUrl: "./tabs-navigation.component.html", // which html doc is this associated to
+    styleUrls: ["./tabs-navigation.component.scss"] // which scss styling doc is this associated to
 })
+
+// the tabs navigation component "tabs-navigation" has an alias in Typescript: TabsNavigationComponent
+// TabsNavigationComponent can be found in app.module.ts, not needed to be touched with Angular CLI working
 export class TabsNavigationComponent implements OnInit {
     constructor(
+        //!required --- these next four lines are chart formatting
         private router: RouterExtensions,
         private active: ActivatedRoute,
         private page: Page
     ) {}
 
+    
     ngOnInit() {
+        // when we start up, load the routes for the 
+        // app to understand where we are going
         this.loadTabRoutes();
+        // check if we are on android
         if (isAndroid) {
             this.page.actionBarHidden = true;
         }
     }
 
+    // how the tabs navigate
     private loadTabRoutes() {
         this.router.navigate(
             [
                 {
                     outlets:
                     {
+                        // if you wish to add another tab, use this format:
+                        // someComponent: ["some-component"],
                         dashboardOverview: ["dashboard-overview"],
                         dashboardCharts: ["dashboard-charts"]
                     }
