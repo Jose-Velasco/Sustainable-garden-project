429d5acd84deeb32c8d999d85863ff5bb17cee35
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2021-10-14 10:48:10 -0700

    Documented all of Dashboard overview

--- src/app/components/dashboard-overview/dashboard-overview.component.ts
+++ src/app/components/dashboard-overview/dashboard-overview.component.ts
@@ -3,42 +3,59 @@ import { Subscription } from "rxjs";
 import { BackendService } from "../../shared/services/backend.service";
 import { SensorsReadingsDataService } from "../../shared/services/sensors-readings-data.service";
 
+// decorator, tells angular and nativescript what component we are talking about
 @Component({
-    selector: "ns-dashboard-overview",
-    templateUrl: "./dashboard-overview.component.html",
-    styleUrls: ["./dashboard-overview.component.scss"]
+    selector: "ns-dashboard-overview", //where in nativescript is this component
+    templateUrl: "./dashboard-overview.component.html", // which html doc is this associated to
+    styleUrls: ["./dashboard-overview.component.scss"] // which scss styling doc is this associated to
 })
+
+// the dashboard overview component "dashboard-overview" has an alias in Typescript: DashboardOverviewComponent
+// DashboardOverviewComponent can be found in app.module.ts, not needed to be touched with Angular CLI working
 export class DashboardOverviewComponent implements OnInit, OnDestroy {
+    // the DashboardOverviewComponent initializes with the following data
+
+    //!required --- these next four lines are chart formatting
     private _currentHumidityValueChanged: Subscription;
     private _currentTemperatureValueChanged: Subscription;
     private _currentRainStatusChanged: Subscription;
+
+    //data used, can be modified
     currentHumidityValue: number;
     currentTemperatureValue: number;
     currentRainStatus: boolean;
-    hasSunLight = false;
+    hasSunLight = false; //!change when we get the data working for the sunlight card
+    
+    //uses sensorReadingDataService and backendService to grab data from the sensors, 
+    //see src/app/shared/services/sensors-readings-data.service.ts and
+    //see src/app/shared/services/backend.service.ts for more information
     constructor(
         private sensorReadingDataService: SensorsReadingsDataService,
         private backendService: BackendService
         ) {}
 
     ngOnInit() {
+        //the changed humidity value is the data from the sensors with the current humidity reading
         this._currentHumidityValueChanged = this.sensorReadingDataService.currentHumidity
             .subscribe(newValue => {
-                this.currentHumidityValue = newValue;
+                this.currentHumidityValue = newValue; //assign the value 
             });
+        //the changed temperature value is the data from the sensors with the current temperature reading
         this._currentTemperatureValueChanged = this.sensorReadingDataService.currentTemperature
             .subscribe(newValue=> {
-                this.currentTemperatureValue = newValue;
+                this.currentTemperatureValue = newValue; //assign the value
             });
+        //the changed rain status value is the data from the sensors with the current rain status reading
         this._currentRainStatusChanged = this.sensorReadingDataService.currentRainStatus
         .subscribe(newValue => {
-            this.currentRainStatus = newValue == 1 ? true : false ;
+            this.currentRainStatus = newValue == 1 ? true : false ; //assign and check the value
         });
 
         // this.backendService.readCurrentSensorValues();
         this.backendService.testDashboardViewUIWithCurrentSensorData();
     }
 
+    // Angular gets rid of the charts when we leave this page
     ngOnDestroy() {
         if (this._currentHumidityValueChanged) {
             this._currentHumidityValueChanged.unsubscribe();
@@ -51,6 +68,7 @@ export class DashboardOverviewComponent implements OnInit, OnDestroy {
         }
     }
 
+    //! could be removed if we change the card to be used with data than a button?
     onSunlightTest(): void {
         this.hasSunLight = !this.hasSunLight;
     }
