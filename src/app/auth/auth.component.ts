c4651b705b2b5bab3cf291d1358c9fbe8d8dcfd4
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2022-01-14 19:56:24 -0800

    Completed task to use the TextFieldObject model for the login page. Code refactoring will start as described in the tasklist within issue #7.
    Closes #13

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { RouterExtensions } from "@nativescript/angular";
 import { TextField } from "@nativescript/core/ui/text-field";
 import { BackendService } from "../shared/services/backend.service";
 import { ChartsDataService } from "../shared/services/charts-data.service";
+import { TextFieldObject } from "../shared/models/textFieldObject.model";
 
 // decorator, tells angular and nativescript what component we are talking about
 @Component({
@@ -12,14 +13,21 @@ import { ChartsDataService } from "../shared/services/charts-data.service";
 })
 // the auth component "auth" has an alias in Typescript: AuthComponent
 // AuthComponent can be found in app.module.ts, not needed to be touched with Angular CLI working  
-export class AuthComponent implements OnInit { // the AuthComponent initializes...
-    //...with the following data
-    testNum: number; //! is this used?
-    username = ""; // placeholder for string username
-    password = ""; // placeholder for string password
-    exisitngUser = "JosephC12"; // an exisiting user for testing
-    existingPassword = "somePassword"; // an exisiting password for testing
-    
+export class AuthComponent implements OnInit {
+    loginTextFieldObjCollection:TextFieldObject[] = new Array();
+
+    username= "";
+    usernameFieldColor: string;
+    usernameFieldOpacity: number;
+    LoginUsernameTextFieldObj = new TextFieldObject(this.username,0);
+
+    password= "";
+    passwordFieldColor: string;
+    passwordFieldOpacity: number;
+    LoginPasswordTextFieldObj = new TextFieldObject(this.password,1);
+    buttonEnabled = false;
+
+
     // TODO: need to set up a server for the exisitng password verification
     // TODO: need to allow sending data to such server
     // TODO: use the logic found in the Login() method inside the server code to verfiy username and password 
@@ -35,55 +43,156 @@ export class AuthComponent implements OnInit { // the AuthComponent initializes.
 
     // where Angular initializes the component, our main() equivalent
     ngOnInit() {
-        this.testNum = 15; //! is this used?
 
         this.backendService.fetchAllSensorsReadings(); // (1)
-        this.chartsDataService.initializeChartServiceData(); //! (2)
+        this.chartsDataService.initializeChartServiceData(); 
         // TODO: "this.chartsDataService.initializeChartServiceData();" has been added here for charts view testing purposes only
-        //!needs to be changed */
+
+        this.loginTextFieldObjCollection.push(this.LoginUsernameTextFieldObj);
+        this.loginTextFieldObjCollection.push(this.LoginPasswordTextFieldObj);
+        
     }
 
     
+    // refactor into TextFieldObject model to be reused everywhere, see new account page for the whole method
+    refreshPagePropsWithObjCollection(someObjCollection:TextFieldObject[],checkAllIfEmpty:boolean,colorChosen:string,inputMode:boolean){
+        let countEmptyFields = 0;
+
+        for (let i= 0; i<someObjCollection.length; i++){
+            let objectInCollection = someObjCollection[i];
+            //let nextObjectInCollection;
+            if(colorChosen == "green"){
+                objectInCollection.changeColorAndOpacitySwitch(false,"green");
+            }
+            //if(someObjCollection[i+1])
+                //nextObjectInCollection = someObjCollection[i+1];
+            if(objectInCollection.getDataOfTextField() == ""){
+                countEmptyFields++;
+                if(checkAllIfEmpty)
+                    objectInCollection.changeColorAndOpacitySwitch(inputMode,colorChosen);
+            }
+            
+            this.pageDataRefresh(objectInCollection);
+
+            if(countEmptyFields==0){
+                this.buttonEnabled = true;
+            }
+            else{
+                this.buttonEnabled = false;
+            }
+            
+        }
+    }
+
+    pageDataRefresh(object:TextFieldObject){
+        if(object == this.LoginUsernameTextFieldObj){
+            console.log("The username on login is changing color");
+            this.usernameFieldColor = object.getColorOfTextField();
+            this.usernameFieldOpacity = object.getOpacityOfTextField();
+        }
+        else if(object == this.LoginPasswordTextFieldObj){
+            console.log("The password on login is changing color");
+            this.passwordFieldColor = object.getColorOfTextField();
+            this.passwordFieldOpacity = object.getOpacityOfTextField();
+        }
+    }
+
+    usernameOnReturnPress(args) {
+        // returnPress event will be triggered when user submits a value
+        let textField = <TextField>args.object;
+
+        // Gets or sets the placeholder text.
+        //console.log(textField.hint);
+        // Gets or sets the input text.
+        this.username = textField.text;
+        this.LoginUsernameTextFieldObj.setDataOfTextField(this.username);
+        
+        console.log("The username is " + textField.text);
+        // Gets or sets the secure option (e.g. for passwords).
+        //console.log(textField.secure);
+
+        // Gets or sets the soft keyboard type. Options: "datetime" | "phone" | "number" | "url" | "email"
+        //console.log(textField.keyboardType);
+        // Gets or sets the soft keyboard return key flavor. Options: "done" | "next" | "go" | "search" | "send"
+        //console.log(textField.returnKeyType);
+        // Gets or sets the autocapitalization type. Options: "none" | "words" | "sentences" | "allcharacters"
+        //console.log(textField.autocapitalizationType);
+
+        // Gets or sets a value indicating when the text property will be updated.
+        //console.log(textField.updateTextTrigger);
+        // Gets or sets whether the instance is editable.
+        //console.log(textField.editable);
+        // Enables or disables autocorrection.
+        //console.log(textField.autocorrect);
+        // Limits input to a certain number of characters.
+        //console.log(textField.maxLength);
+
+        this.LoginUsernameTextFieldObj.changeColorAndOpacitySwitch(false,"red");
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"red",false);
+
+        setTimeout(() => {
+            textField.dismissSoftInput(); // Hides the soft input method, ususally a soft keyboard.
+        }, 100);
+    }
+
+    usernameOnFocus(args) {
+        // focus event will be triggered when the users enters the TextField
+        let textField = <TextField>args.object;
+        this.LoginUsernameTextFieldObj.changeColorAndOpacitySwitch(true,"red");
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"red",true);
+    }
+
+    usernameOnBlur(args) {
+        // blur event will be triggered when the user leaves the TextField
+        let textField = <TextField>args.object;
+        this.username = textField.text;
+        this.LoginUsernameTextFieldObj.setDataOfTextField(this.username);
+        
+        console.log("The username is " + textField.text);
+
+        this.LoginUsernameTextFieldObj.changeColorAndOpacitySwitch(false,"red");
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"red",false);
+    }
+
+    passwordOnReturnPress(args) {
+        // returnPress event will be triggered when user submits a value
+        let textField = <TextField>args.object;
+
+        this.password = textField.text;
+        this.LoginPasswordTextFieldObj.setDataOfTextField(this.password);
+        
+        console.log("The password is " + textField.text);
+
+        this.LoginPasswordTextFieldObj.changeColorAndOpacitySwitch(false,"red");
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"red",false);
+
+        setTimeout(() => {
+            textField.dismissSoftInput(); // Hides the soft input method, ususally a soft keyboard.
+        }, 100);
+    }
+
+    passwordOnFocus(args) {
+        // focus event will be triggered when the users enters the TextField
+        let textField = <TextField>args.object;
+        this.LoginPasswordTextFieldObj.changeColorAndOpacitySwitch(true,"red");
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"red",true);
+    }
+
+    passwordOnBlur(args) {
+        // blur event will be triggered when the user leaves the TextField
+        let textField = <TextField>args.object;
+        this.password = textField.text;
+        this.LoginPasswordTextFieldObj.setDataOfTextField(this.password);
+        
+        console.log("The password is " + textField.text);
+
+        this.LoginPasswordTextFieldObj.changeColorAndOpacitySwitch(false,"red");
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"red",false);
+
+    }
+
     Login() {
-        // TODO, create a server with docker to verfiy usernames and passwords
-        // Note: a new service in the service folder needed to send data with HTTP request,
-        //   
-        /*
-        let options = {
-            title: "Invalid Login",
-            message: "Username/Email was not found in database.",
-            okButtonText: "OK"
-        };
-        
-        alert(options).then(() => {
-            console.log("Race chosen!");
-        });
-
-        for dialog component in ui under shared
-        */
-        // the user's username input does not match any username in the database
-        /*if (this.username != this.exisitngUser){
-            // TODO, connect a server with data to check the users with HTTP request and docker
-            console.log(this.username);
-            console.log("Invalid input: username/email was not found in database");
-
-            // dialog, invalid input: username/email was not found in database
-        }
-        
-        // the user has a username in the database, but the password doesnt match
-        else if (this.username == this.exisitngUser && this.existingPassword != this.password) {
-            // TODO, connect a server with data to check the password with HTTP request
-            console.log("Invalid input: password was not found in database")
-        }
-
-        // else, the user that has a username in the database and has matched with their password, login
-        else {
-            this.router.navigate(["tabs"], { clearHistory: true});
-            this.backendService.fetchAllSensorsReadings();
-            this.chartsDataService.initializeChartServiceData();
-            console.log("loggedIn");
-        }
-        */
+        this.refreshPagePropsWithObjCollection(this.loginTextFieldObjCollection,false,"green",false);
         
         this.router.navigate(["tabs"], { clearHistory: true});  
         // uses the RouterExtentions from constructor to navigate to tabs, see "tabs" in
@@ -96,91 +205,5 @@ export class AuthComponent implements OnInit { // the AuthComponent initializes.
     createNewAccount(){
         this.router.navigate(["newAccountPage"], { clearHistory: true});
         console.log("creatingAccount");
-    }
-
-    usernameOnReturnPress(args) {
-        // returnPress event will be triggered when user submits a value
-        let textField = <TextField>args.object;
-
-        // Gets or sets the placeholder text.
-        //console.log(textField.hint);
-        // Gets or sets the input text.
-        console.log("The username is " + textField.text);
-        textField.text = this.username;
-        // Gets or sets the secure option (e.g. for passwords).
-        //console.log(textField.secure);
-
-        // Gets or sets the soft keyboard type. Options: "datetime" | "phone" | "number" | "url" | "email"
-        //console.log(textField.keyboardType);
-        // Gets or sets the soft keyboard return key flavor. Options: "done" | "next" | "go" | "search" | "send"
-        //console.log(textField.returnKeyType);
-        // Gets or sets the autocapitalization type. Options: "none" | "words" | "sentences" | "allcharacters"
-        //console.log(textField.autocapitalizationType);
-
-        // Gets or sets a value indicating when the text property will be updated.
-        //console.log(textField.updateTextTrigger);
-        // Gets or sets whether the instance is editable.
-        //console.log(textField.editable);
-        // Enables or disables autocorrection.
-        //console.log(textField.autocorrect);
-        // Limits input to a certain number of characters.
-        //console.log(textField.maxLength);
-
-        setTimeout(() => {
-            textField.dismissSoftInput(); // Hides the soft input method, ususally a soft keyboard.
-        }, 100);
-    }
-
-    usernameOnFocus(args) {
-        // focus event will be triggered when the users enters the TextField
-        let textField = <TextField>args.object;
-    }
-
-    usernameOnBlur(args) {
-        // blur event will be triggered when the user leaves the TextField
-        let textField = <TextField>args.object;
-    }
-
-    onReturnPress(args) {
-        // returnPress event will be triggered when user submits a value
-        let textField = <TextField>args.object;
-
-        // Gets or sets the placeholder text.
-        //console.log(textField.hint);
-        // Gets or sets the input text.
-        console.log("The password is " + textField.text);
-        textField.text = this.password;
-        // Gets or sets the secure option (e.g. for passwords).
-        //console.log(textField.secure);
-
-        // Gets or sets the soft keyboard type. Options: "datetime" | "phone" | "number" | "url" | "email"
-        //console.log(textField.keyboardType);
-        // Gets or sets the soft keyboard return key flavor. Options: "done" | "next" | "go" | "search" | "send"
-        //console.log(textField.returnKeyType);
-        // Gets or sets the autocapitalization type. Options: "none" | "words" | "sentences" | "allcharacters"
-        //console.log(textField.autocapitalizationType);
-
-        // Gets or sets a value indicating when the text property will be updated.
-        //console.log(textField.updateTextTrigger);
-        // Gets or sets whether the instance is editable.
-        //console.log(textField.editable);
-        // Enables or disables autocorrection.
-        //console.log(textField.autocorrect);
-        // Limits input to a certain number of characters.
-        //console.log(textField.maxLength);
-
-        setTimeout(() => {
-            textField.dismissSoftInput(); // Hides the soft input method, ususally a soft keyboard.
-        }, 100);
-    }
-
-    onFocus(args) {
-        // focus event will be triggered when the users enters the TextField
-        let textField = <TextField>args.object;
-    }
-
-    onBlur(args) {
-        // blur event will be triggered when the user leaves the TextField
-        let textField = <TextField>args.object;
     }
 }
