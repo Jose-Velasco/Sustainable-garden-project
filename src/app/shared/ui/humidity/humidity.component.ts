ebb76afee9c1bc0c8dd5d117ec17a7c8e5326651
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2022-05-22 21:17:11 -0700

    UI Changes for dashboard and side bar

--- src/app/shared/ui/humidity/humidity.component.ts
+++ src/app/shared/ui/humidity/humidity.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input } from "@angular/core";
+import { Enums, Label } from "@nativescript/core";
 
 @Component({
     selector: "ns-humidity",
@@ -8,4 +9,23 @@ import { Component, Input } from "@angular/core";
 export class HumidityComponent {
     @Input() humidityValue: number;
     constructor() {}
+    
+    calculateHumidAniValue(humidityValue: number): number {
+        let convertedAnimationValue: number;
+        if (humidityValue > 100) {
+            convertedAnimationValue = 0;
+        } else if (humidityValue < 0) {
+            convertedAnimationValue = 100
+        }
+        return convertedAnimationValue;
+    }
+
+    private animateDroplet(labelElement: ElementRef<Label>, humidityValue: number): void {
+        labelElement.nativeElement.animate({
+            translate: {x: 0, y: this.calculateHumidAniValue(humidityValue)},
+            duration: 500,
+            curve: Enums.AnimationCurve.easeIn
+        })
+    }
+    
 }
--- src/app/shared/ui/soil/soil.component.ts
+++ src/app/shared/ui/soil/soil.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input } from "@angular/core";
+import { Enums, Label } from "@nativescript/core";
 
 @Component({
     selector: "ns-soil",
@@ -7,7 +8,27 @@ import { Component, Input } from "@angular/core";
 })
 export class SoilComponent {
     @Input() soilValue: number;
+    soilColor: number;
     constructor() {}
 
+    
+    calculateSoilMoistureValue(soilValue: number): number {
+        let convertedAnimationValue: number;
+        if (soilValue > 100) {
+            convertedAnimationValue = 0;
+        } else if (soilValue < 0) {
+            convertedAnimationValue = 100;
+        }
 
+        return convertedAnimationValue
+    }
+
+    private animateDroplet(labelElement: ElementRef<Label>, soilValue: number): void {
+        labelElement.nativeElement.animate({
+            translate: {x: 0, y: this.calculateSoilMoistureValue(soilValue)},
+            duration: 500,
+            curve: Enums.AnimationCurve.easeIn
+        })
+    }
+ 
 }
